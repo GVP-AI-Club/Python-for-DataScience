6e3322d791111852278d84b4123125a1752d11fd
RAKESH PEELA <rakhi2104@gmail.com>
2018-07-07 05:19:07 +0530

    added headers

--- Basics/script-2.sh
+++ Basics/script-2.sh
@@ -1,4 +1,9 @@
+#!/bin/bash -       
+#title           : script-2.sh
+#description     : This script installes the pre-requisite packages for Machine Learning and Deep Learning
+#author		       : Rakesh Peela (@rakhi2104)
+#date            : 07-12-2017
+#usage		       : sh script-2.sh
+
 apt-get install python-dev -y
 pip install jupyter tensorflow numpy scipy scikit-learn pillow h5py keras matplotlib
-
-

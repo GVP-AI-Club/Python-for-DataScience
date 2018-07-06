a369261ca7d4e7194506ee91af0f32feaedafc53
RAKESH PEELA <rakhi2104@gmail.com>
2018-07-07 05:18:22 +0530

    added headers

--- Basics/script-1.sh
+++ Basics/script-1.sh
@@ -1,3 +1,10 @@
+#!/bin/bash -       
+#title           : script-1.sh
+#description     : This script installes the pre-requisite packages for Machine Learning and Deep Learning
+#author		       : Rakesh Peela (@rakhi2104)
+#date            : 07-12-2017
+#usage		       : sh script-1.sh
+
 apt-get update
 apt-get install python-pip
 apt-get purge -y python-pip
